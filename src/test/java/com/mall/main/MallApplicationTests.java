ba457a63edec7f04987fdc80543587bc942c4f83
JungSoy <121956754+JungSoy@users.noreply.github.com>
2024-01-15 17:09:42 +0900

    20240115

---
+++ src/test/java/com/mall/main/MallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
